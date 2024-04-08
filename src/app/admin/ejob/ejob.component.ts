55aa4b1b894d185325c3fb0bbe9b9f0264f6dcd9
Akhil Madutha <aakhi53@gmail.com>
2024-04-08 16:57:04 +0530

    Ui Changes

--- src/app/admin/ejob/ejob.component.ts
+++ src/app/admin/ejob/ejob.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef  } from '@angular/core';
 import { QuillEditorComponent, QuillModule } from 'ngx-quill';
 
 import { TranslateService } from '@ngx-translate/core';
@@ -12,11 +12,24 @@ import { DirectionService } from '../../shared/services/direction.service';
 export class EjobComponent implements AfterViewInit  {
   // Declare a ViewChild to access the Quill editor component
   @ViewChild('editor', { static: true }) editorComponent: QuillEditorComponent;
+  @ViewChild('fileInput', { static: false }) fileInput: ElementRef<HTMLInputElement>;
 
   files: File[] = [];
 
   onFileSelected(event: any): void {
-    this.files = Array.from(event.target.files);
+    const filesToAdd: File[] = Array.from(event.target.files as FileList);
+    this.files.push(...filesToAdd);
+    event.target.value = ''; // Reset file input to allow the same file to be selected again
+  }
+
+  deleteFile(index: number): void {
+    this.files.splice(index, 1);
+  }
+
+  uploadFiles(): void {
+    // Placeholder for upload logic
+    console.log('Uploading', this.files);
+    // Here you would typically call a service to upload the files to a server
   }
   
   constructor(private translate: TranslateService,
